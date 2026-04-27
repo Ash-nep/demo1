caf5029b239d669e9b817738bd6bd001d3050325
Ash-nep <asharma.it.nepal@gmail.com>
2026-04-27 16:04:29 +0545

    Update files and add new widgets

---
+++ lib/gradient_container.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:demo1/styled_text.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+const GradientContainer ({super.key, required this.colors});
+
+final List<Color>colors;
+ @override
+  Widget build(context) {
+    
+    return Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: colors,
+              begin: startAlignment,
+              end: endAlignment,
+            ),
+          ),
+          child: Center(
+            child: StyledText('Hello world'),
+          ),
+        );
+  }
+}
